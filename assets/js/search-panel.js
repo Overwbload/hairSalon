c0b805c3f03208b3b4dfb940daca4e2eb544cced
Ivan <swalok47716@gmail.com>
2023-12-03 01:53:15 +0800

    [ refactor ] 重構header地區資料結構

--- assets/js/search-panel.js
+++ assets/js/search-panel.js
@@ -1,5 +1,9 @@
-const searchRegions = ["northern", "central", "southern", "outside-of-mainland"];
-const searchAreas = ["keelong", "yilan", "taipei", "taoyuan", "hsinchu"];
+const searchTaiwan = {
+  "northern": ["keelong", "yilan", "taipei", "taoyuan", "hsinchu"],
+  "central": [""],
+  "southern": [""],
+  "outside-of-mainland": [""]
+}
 
 $(document).ready(function(){
   // init
@@ -17,37 +21,4 @@ $(document).ready(function(){
     $(".search-panel-content").hide();
   });
 
-  // 
-  searchRegions.forEach(region => {
-    $(`.${region}-region, .search-area`).hover(function() {
-      $(".search-area").show();
-      // $(`.${region}-area`).show();
-    }, function() {
-      $(".search-area").hide();
-      // $(`.${region}-area`).hide();
-    });
-  });
-
-  // 
-  searchAreas.forEach(area => {
-    // $(`.${area}-area, .search-city`).hover(function() {
-    //   console.log(`hover ${area}`);
-    //   $(`${area}-cities`).show();
-    //   $(".search-city").show();
-    // }, function() {
-    //   $(`${area}-cities`).hide();
-    //   $(".search-city").hide();
-    // });
-    $(`.${area}-area, .${area}-cities`).hover(function() {
-      $(`.${area}-cities`).show();
-      $(".search-city").show();
-      // $(`.${area}-area`).show();
-      $(".search-area").show();
-    }, function() {
-      $(`.${area}-cities`).hide();
-      $(".search-city").hide();
-      // $(`.${area}-area`).hide();
-      $(".search-area").hide();
-    });
-  });
 });
