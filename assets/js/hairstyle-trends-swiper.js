d2f59f8e2453259ba2135df980eb7d9c1700023e
Ivan <swalok47716@gmail.com>
2023-12-12 15:06:21 +0800

    [ feat ] 新增首頁圖庫及改善首頁排版
    1. 新增最新趨勢圖庫
    2. 改善首頁第五部分排版

--- assets/js/hairstyle-trends-swiper.js
+++ assets/js/hairstyle-trends-swiper.js
@@ -3,10 +3,11 @@ const trendingModels = [
   ["person-5-resized.png", "person-6-resized.png"],
   ["person-7-resized.png", "person-8-resized.png"],
   ["person-9-resized.png", "person-10-resized.png"],
-  ["person-3-resized.png", "person-4-resized.png"],
-  ["person-5-resized.png", "person-6-resized.png"],
-  ["person-7-resized.png", "person-8-resized.png"],
-  ["person-9-resized.png", "person-10-resized.png"],
+  ["person-12.jpg", "person-13.jpg"],
+  ["person-14.jpg", "person-15.jpg"],
+  ["person-16.jpg", "person-17.jpg"],
+  ["person-18.jpg", "person-19.jpg"],
+  ["person-20.jpg", "person-21.jpg"],
 ]
 
 const swiperWrapper = document.querySelector(".hairstyle-trends-swiper .swiper-wrapper");
