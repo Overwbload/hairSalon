6f519221387d7570cdb72b2e6025859d4d6cc050
Ivan <swalok47716@gmail.com>
2023-12-21 03:31:14 +0800

    [ style ] 改善檔案命名
    1. 落實camelCase於檔案名
    2. 子內容可以使用underscore "_" 連接

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import '@scss/all.scss';
 import '@node_modules/bootstrap/dist/js/bootstrap.min.js';
 
-import '@js/area-search-panel.js';
-import '@js/style-search-panel.js';
+import '@js/header_area.js';
+import '@js/header_style.js';
 import '@js/preloader';
 
 console.log("Hello world!");
