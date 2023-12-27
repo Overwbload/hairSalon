46b7cf3821f62c07d4e951337db5ce1192db63bd
YI-JYUN HE <jobs.steve54@gmail.com>
2023-12-27 12:49:15 +0800

    [ build ] 安裝" firebase "到 npm
    執行安裝 "npm install firebase"

---
+++ assets/js/firbaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD39n48j8Qon0Bd10QtG83mZFZfaZ1wor8",
+    authDomain: "hairsalon-18a70.firebaseapp.com",
+    projectId: "hairsalon-18a70",
+    storageBucket: "hairsalon-18a70.appspot.com",
+    messagingSenderId: "648480886492",
+    appId: "1:648480886492:web:b110b92a424a425e296e19"
+  };
